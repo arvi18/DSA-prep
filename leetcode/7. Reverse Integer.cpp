40270acfb6eb390ba8f8b1ac8ed5e9230fbf9f81
Arvind Shelke <ucdonor5@gmail.com>
2023-06-25 17:28:24 +0530

    medium 7. Reverse Integer

---
+++ leetcode/7. Reverse Integer.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << endl;
+
+    class Solution
+    {
+    public:
+        int reverse(int x)
+        {
+
+            int ans = 0;
+            while (x != 0)
+            {
+                int digit = x % 10;
+                if (ans > INT_MAX / 10 || ans < INT_MIN / 10)
+                {
+                    return 0;
+                }
+                ans = ans * 10 + digit;
+                x /= 10;
+            }
+            return ans;
+        }
+    };
+
+    cout << endl
+         << endl;
+    return 0;
+}
