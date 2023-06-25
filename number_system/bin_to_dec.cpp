41c4d6fd665b103b23032a3754672a8b4ce9d77f
Arvind Shelke <ucdonor5@gmail.com>
2023-06-25 15:58:53 +0530

    using LL and bin to decimal

--- number_system/bin_to_dec.cpp
+++ number_system/bin_to_dec.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 string bin_to_dec(int n)
 {
     int start;
     string num = "";
+    if (n == 0)
+        return "0";
     while (n != 0)
     {
         start = n % 2;
@@ -18,9 +21,17 @@ string bin_to_dec(int n)
 
 int main()
 {
+    cout << endl;
     int n;
     cin >> n;
 
-    cout << bin_to_dec(n) << endl;
+    cout << "Binary of " << n << " is: " << bin_to_dec(n);
+
+    // ll max = INT_MAX, min = INT_MIN;
+    // cout << max << " " << min << " " << abs(min) - abs(max);
+
+    cout << endl
+         << endl;
+
     return 0;
 }
